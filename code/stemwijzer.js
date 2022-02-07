495a3bc5713bd3432dc8c555df1bfda2066d4204
Rick Huisman <99062653@mydavinci.nl>
2022-02-07 14:46:52 +0100

    0.055

--- code/stemwijzer.js
+++ code/stemwijzer.js
@@ -1,7 +1,9 @@
 var _startpagina = document.getElementById("start-container");
 var _vragenpagina = document.getElementById("vragen-container");
+var _amount = document.getElementById("amount");
+_amount.innerHTML = subjects.length;
 
 function startQuestions() {
-    
+    console.log(subjects);
 
 }
