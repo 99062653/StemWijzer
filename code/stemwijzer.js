d832fe87d298fe6d510663e15c437f166d3e0293
Rick Huisman <99062653@mydavinci.nl>
2022-02-08 12:21:34 +0100

    0.19

--- code/stemwijzer.js
+++ code/stemwijzer.js
@@ -6,21 +6,24 @@ var _counter = document.getElementById("count-vragen");
 var _titel = document.getElementById("titel-vragen");
 var _subtitel = document.getElementById("subtitel-vragen");
 
-function stemwijzerQuestions() {
-    var _index = 0;
+var _index = 0;
 
-    _startpagina.style.visibility = "hidden";
-    _vragenpagina.style.visibility = "visible";
-    loadQuestions(_index)
+function stemwijzerMain() {
+    if (_index >= 0) {
+        _startpagina.style.visibility = "hidden";
+        _vragenpagina.style.visibility = "visible";
+        
+        loadQuestions(_index)
+    } else {
+        _vragenpagina.style.visibility = "hidden";
+        _startpagina.style.visibility = "visible";
 
+        _index = 0;
+    }
 }
 
 function loadQuestions(index) {
-    if (index == 0) {
-        index = 1;
-    } else {
-        index = index;
-    }
+    index == 0 ? index = 1 : index; //ternary operater: soortgelijk aan if else; ? = true, : = else
 
     var _progressCalculated = index / subjects.length * 100
 
@@ -28,4 +31,10 @@ function loadQuestions(index) {
     _counter.innerText = index + " / " + subjects.length;
     _titel.innerText = subjects[index].title
     _subtitel.innerText = subjects[index].statement
+}
+
+function questionBack() {
+    _index--
+    
+    stemwijzerMain()
 }
