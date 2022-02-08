a1250cad7cf4b562a495412ca9603d80cdd685ff
Rick Huisman <99062653@mydavinci.nl>
2022-02-08 11:40:35 +0100

    0.1

--- code/stemwijzer.js
+++ code/stemwijzer.js
@@ -1,13 +1,28 @@
 var _startpagina = document.getElementById("start-container");
 var _vragenpagina = document.getElementById("vragen-container");
 
+var _progress = document.getElementById("progress-vragen");
+var _counter = document.getElementById("count-vragen");
 var _titel = document.getElementById("titel-vragen");
 var _subtitel = document.getElementById("subtitel-vragen");
 
-function startQuestions() {
-    console.log(subjects);
+function stemwijzerQuestions() {
+    var _index = 0;
     
-    _startpagina.style.visibility = "hidden";
-    _vragenpagina.style.visibility = "visible"
+    if (_index == 0) {
+        _startpagina.style.visibility = "hidden";
+        _vragenpagina.style.visibility = "visible";
+        loadQuestions(_index)
+    } else {
+        _vragenpagina.style.visibility = "hidden";
+        _startpagina.style.visibility = "visible";
+    }
+}
 
+function loadQuestions(index) {
+    console.log(subjects[index])
+
+    _counter.innerText = (index + 1) + " / " + subjects.length;
+    _titel.innerText = subjects[index].title
+    _subtitel.innerText = subjects[index].statement
 }
